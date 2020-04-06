b114bf03717be31f4b9b1a5d7b0e163ec1165212
Gyuhwan Park★ <doping.cheese@gmail.com>
2020-04-06 23:44:35 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const SDL2 = require('node-sdl2');
+const Application = SDL2.app;
+const Window = SDL2.window;
+const Image = SDL2.image;
+const Font = SDL2.font;
+
+const SCREEN_WIDTH  = 480;
+const SCREEN_HEIGHT = 320;
+
+
+const backgroundImage = new Image(__dirname + '/background.png'); 
+const font = new Font(__dirname + '/goyangNJ.ttf', 32);
+
+
+const mainWindow = new Window({ w: SCREEN_WIDTH, h: SCREEN_HEIGHT, borderless: true });
+
+const draw = () => {
+    const { render } = mainWindow;
+    const { outputSize } = render;
+
+    render.fillRect([[0, 0, SCREEN_WIDTH, SCREEN_HEIGHT]]);
+    render.copy(backgroundImage.texture(render), null, [0, 0, outputSize.w, outputSize.h]);
+
+    // 시간 표시
+    const currentTime = new Date().toLocaleTimeString();
+    const fontSize = font.getSize(currentTime);
+    const text = font.blend(currentTime, 0xffffff);
+    render.copy(text.texture(render), null, [outputSize.w - fontSize.w - 8, 0, fontSize.w, fontSize.h]);
+    
+    // 새 프레임 렌더
+    render.present();
+};
+
+mainWindow.on('close', () => {
+    Application.quit();
+});
+
+mainWindow.on('change', () => {
+    draw();
+});
+
+setInterval(() => draw(), 1000 / 60);
